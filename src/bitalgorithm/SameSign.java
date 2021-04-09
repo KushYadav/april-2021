56ab9919856c75109afcf97baa2162c973406a3a
KushYadav <kushyadavnitd@gmail.com>
2021-04-09 19:12:17 +0530

    opposite sign

---
+++ src/bitalgorithm/SameSign.java
@@ -0,0 +1,15 @@
+package bitalgorithm;
+
+public class SameSign {
+
+	public static void main(String[] args) {
+
+		int x = 100, y = -100;
+		if ((x ^ y) < 0) {
+			System.out.println("Opposite Sign");
+		} else {
+			System.out.println("Same Sign");
+		}
+	}
+
+}
