4a84dbeb3e31938c19fe976bd419cf49317ccf0e
KushYadav <kushyadavnitd@gmail.com>
2021-04-17 20:01:38 +0530

    BST

---
+++ src/bst/BST.java
@@ -0,0 +1,113 @@
+package bst;
+
+public class BST {
+
+	private class Node {
+		int data;
+		Node left;
+		Node right;
+	}
+
+	// Required Variables
+
+	private Node root;
+	private int size;
+
+	// Constructing tree using an array
+
+	private Node construct(int[] arr, int lo, int hi) {
+		if (lo > hi) {
+			return null;
+		}
+		int mid = (lo + hi) / 2;
+		Node node = new Node();
+		node.data = arr[mid];
+		node.left = construct(arr, lo, mid - 1);
+		node.right = construct(arr, mid + 1, hi);
+		return node;
+	}
+
+	public void construct(int[] arr) {
+		root = construct(arr, 0, arr.length - 1);
+		size = arr.length;
+	}
+
+	// Basic Functions
+
+	private void display(Node node) {
+		if (node == null) {
+			return;
+		}
+		System.out.print(node.left == null ? " " : node.left.data);
+		System.out.print("<=" + node.data + "=>");
+		System.out.println(node.right == null ? " " : node.right.data);
+		display(node.left);
+		display(node.right);
+	}
+
+	public void display() {
+		display(root);
+	}
+
+	public boolean isEmpty() {
+		return size == 0 ? true : false;
+	}
+
+	public int size() {
+		return size;
+	}
+
+	private int sizeR(Node node) {
+		if (node == null) {
+			return 0;
+		}
+		return sizeR(node.left) + sizeR(node.right) + 1;
+	}
+
+	public int sizeR() {
+		return sizeR(root);
+	}
+
+	private int height(Node node) {
+		if (node == null) {
+			return -1;
+		}
+		return Math.max(height(node.left), height(node.right)) + 1;
+	}
+
+	public int height() {
+		return height(root);
+	}
+
+	public int min() {
+		Node trav = root;
+		while (trav.left != null) {
+			trav = trav.left;
+		}
+		return trav.data;
+	}
+
+	public int max() {
+		Node trav = root;
+		while (trav.right != null) {
+			trav = trav.right;
+		}
+		return trav.data;
+	}
+
+	public boolean find(int data) {
+		Node trav = root;
+		while (trav != null) {
+			if (trav.data == data) {
+				return true;
+			}
+			if (data > trav.data) {
+				trav = trav.right;
+			} else {
+				trav = trav.left;
+			}
+		}
+		return false;
+
+	}
+}
---
+++ src/bst/ClientBST.java
@@ -0,0 +1,17 @@
+package bst;
+
+public class ClientBST {
+	public static void main(String[] args) {
+
+		int[] arr = new int[] { 11, 23, 25, 30, 31, 35, 42, 50, 70, 73, 80, 85 };
+		BST bst = new BST();
+		bst.construct(arr);
+		bst.display();
+//		System.out.println(bst.size());
+//		System.out.println(bst.sizeR());
+//		System.out.println(bst.height());
+//		System.out.println(bst.min());
+//		System.out.println(bst.max());
+//		System.out.println(bst.find(23));
+	}
+}
