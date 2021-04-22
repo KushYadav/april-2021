0e709d6f28c5f20adb1bfe9c129e4db2c92a4046
KushYadav <kushyadavnitd@gmail.com>
2021-04-22 11:36:01 +0530

    no consecutive buildings

--- src/dp/NoConsecutiveBuildingOppRoad.java
+++ src/dp/NoConsecutiveBuildingOppRoad.java
@@ -3,7 +3,18 @@ package dp;
 public class NoConsecutiveBuildingOppRoad {
 
 	public static void main(String[] args) {
+		int n = 6;
+		int building = 0;
+		int space = 0;
 
-		
+		for (int i = 1; i <= n; i++) {
+			if (i == 1) {
+				building = space = 1;
+			} else {
+				building = space + (space = building);
+			}
+		}
+		int combinations = building + space;
+		System.out.println(combinations * combinations);
 	}
 }
